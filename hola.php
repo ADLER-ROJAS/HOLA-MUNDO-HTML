e2107309057ab7290c82b50acaffe08b6718bea5
ADLER-ROJAS <1378509@senati.pe>
2022-10-27 18:54:01 -0500

    Xd

---
+++ hola.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <html lang="es">
+        <head>
+        <title>MI RIMERO PROGRAMA </title>ttle>
+    </head>
+    <body>
+    	        Esto es HTML
+
+                <?php
+
+                    echo "hola mundo, esto es php"
+
+                ?>	
+    </body> 
+    </html>
